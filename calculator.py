a38f32e319a96c3756a474741501891687bb076b
Omer Ahmad <soulcraftsucks@gmail.com>
2026-06-14 19:16:57 +0500

    lecture 0 - functions, f-strings, input validation

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def main():
+    x = int(input("Whats X? "))
+    print(f"X squared is {square(x)}")
+
+def square(n):
+    return n * n
+
+
+
+main()
+
---
+++ name.py
@@ -0,0 +1,8 @@
+# Ask the user for their name and age
+name = input("whats your name? ")
+name = name.strip()
+name = name.title()
+age = input("whats your age? ")
+age = age.strip()
+print(f"my name is {name} and I am {age} years old")
+
