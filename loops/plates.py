d2a7ca05088f66894cbf2c5554d459ce852b768e
Omer Ahmad <soulcraftsucks@gmail.com>
2026-06-17 14:48:47 +0500

    updated

--- loops/plates.py
+++ loops/plates.py
@@ -13,6 +13,30 @@ def is_valid(plate):
         return False
     elif not plate.isalnum():
         return False
+    number_started = False
+    for c in plate:
+        if not number_started and c == "0":
+            return False
+        if c.isdigit():
+            number_started = True
+        if number_started and c.isalpha():
+            return False
+    return True
+        
+    
+
+if __name__ == "__main__":
+    main()
+
+        
+            
+        
+    
+        
+    
+
+    
+
         
         
 
