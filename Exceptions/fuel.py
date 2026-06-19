7ee0eedda55bf4930085e7d7e5d622015c73eb4a
Omer Ahmad <soulcraftsucks@gmail.com>
2026-06-19 16:45:15 +0500

    problem set 1

---
+++ Exceptions/fuel.py
@@ -0,0 +1,33 @@
+def main():
+    initial_input = converter()
+    if initial_input >= 99:
+        print("F")
+    elif initial_input <= 1:
+        print("E")
+    else:
+        print(f"{initial_input}%")
+
+def converter():
+    while True:
+        try:
+            x, y = input("Fraction: ").split("/")
+            if int(x) >= 0 and int(y) != 0 and int(x) <= int(y):
+                z = round(int(x)/int(y)*100)
+                return z
+            
+            
+            
+
+        except ValueError:
+            pass
+        except ZeroDivisionError:
+            pass
+        
+main()
+        
+
+    
+
+    
+        
+    
