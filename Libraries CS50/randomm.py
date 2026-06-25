2ee5195a724362bccf01f243e0571a233b4d343e
Omer Ahmad <soulcraftsucks@gmail.com>
2026-06-25 21:36:57 +0500

    save

--- Libraries CS50/randomm.py
+++ Libraries CS50/randomm.py
@@ -1,8 +1,8 @@
 import sys
 
-if len(sys.argv[1]) < 2:
+if len(sys.argv) < 2:
     print("too few arguments")
-elif len(sys.argv[1]) > 2:
+elif len(sys.argv) > 2:
     print("too many arguments")
 else:
     print("Hello, my name is", sys.argv[1])
