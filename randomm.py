88a072de80c59d740b7f7f20e19f5feb8a91761f
Omer Ahmad <soulcraftsucks@gmail.com>
2026-06-26 21:20:31 +0500

    libraries

--- randomm.py
+++ randomm.py
@@ -1,10 +1,16 @@
+import json
+import requests 
 import sys
 
-if len(sys.argv) < 2:
-    print("too few arguments")
-elif len(sys.argv) > 2:
-    print("too many arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
+if len(sys.argv) != 2:
+    sys.exit()
+
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
+
 
     
