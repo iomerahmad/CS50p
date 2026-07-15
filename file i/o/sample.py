a6372d96dd63f0d6c064d33009ea17b77710e209
Omer Ahmad <soulcraftsucks@gmail.com>
2026-07-15 22:55:42 +0500

    problem set 1

---
+++ file i/o/sample.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def main():
+    if len(sys.argv) > 2:
+        sys.exit("too many command in line arguments!")
+    elif len(sys.argv) < 2:
+        sys.exit("Too few command in line argument")
+    elif len(sys.argv) == 2:
+        if sys.argv[1].endswith(".py"):
+            line = 0
+            name = sys.argv[1]
+            with open(name, "r") as file:
+                for lines in file:
+                    if lines.lstrip().startswith("#"):
+                        pass
+                    elif lines.lstrip().startswith(""):
+                        pass
+                    else:
+                        line += 1
+        else:
+            sys.exit("File not found!")
+    print(line)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+    
